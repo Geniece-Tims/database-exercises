6ee4a2db9f147ac4be95d22a2b1b5b62630fe4ea
Geniece Tims <geniece.c.tims@gmail.com>
2023-07-20 22:05:09 -0500

    complete more exercises for mysql

---
+++ functions_exercises.sql
@@ -0,0 +1,23 @@
+USE employees;
+
+SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'e%e';
+
+SELECT * FROM employees WHERE MONTH(birth_date) = 12 AND DAY(birth_date) = 25;
+SELECT * FROM employees WHERE MONTH(birth_date) = 12 AND DAY(birth_date) = 25 AND YEAR(hire_date) BETWEEN 1990 AND 1999;
+
+SELECT *
+FROM employees
+WHERE MONTH(birth_date) = 12
+    AND DAY(birth_date) = 25
+    AND YEAR(hire_date) BETWEEN 1990 AND 1999
+ORDER BY birth_date ASC, hire_date DESC;
+
+SELECT DATEDIFF(CURDATE(), hire_date) as 'Hired Days Ago', CONCAT(first_name,
+    ' ', last_name) AS full_name
+FROM employees
+WHERE MONTH(birth_date) = 12
+    AND DAY(birth_date) = 25
+    AND YEAR(hire_date) BETWEEN 1990 AND 1999
+ORDER BY full_name DESC;
+
+
